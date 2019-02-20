3406789e6a3f52e5d252ca26e51d9d2f2465d1c3
himadrikundu <kundu.himadri@gmail.com>
2019-02-20 23:34:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -27,10 +27,10 @@ io.sockets.on('connection', function (socket, username) {
     }); 
 });
 
-//app.listen(8080);
+app.listen(8080);
 
-app.listen(8080, '9.251.54.221', function() {
+/*app.listen(8080, '9.251.54.221', function() {
     console.log('Listening to port:  ' + 8080);
-});
+});*/
 
 
